07b44c18e34ee25f28d69ec9c96e984a00cd8572
Kristina <kristina3138040@gmail.com>
2020-07-26 18:57:44 +0300

    Add config file, read data from config file

--- main.py
+++ main.py
@@ -1,39 +1,60 @@
-#192.168.1.1
+#!/usr/bin/env python3
+
 import socket
-import codecs
+import yaml
 
 MAX_DATAGRAM_LEN = 512
 DEAFAUL_PORT = 53
-UDPHEADER_SIZE = 8
 DATAGRAM_SIZE = 41
+
+def isDomainInBlackList(domainName, blacklist, banMsg):
+	if domainName in blacklist:
+		print("\"", domainName, "\"", banMsg)
+		return True
+	return False
+
+def readSettings(domainName):
+	with open('DNSProxyServer.conf') as f:
+		config = yaml.safe_load(f)
+
+	blacklist = config["blacklist"]
+	print("blacklist: ", blacklist)
+
+	superiorDNSServer = config["superiorDNSServer"]
+	print("superiorDNSServer: ", superiorDNSServer)
+
+	banMsg = config["banMsg"]
+	print("banMsg: ", banMsg)
+
+	isInBlackList(domainName, blacklist, banMsg)
 
 def getDomainName(datagram):
 	print(type(datagram), " len: ", len(datagram))
 
 	domainNameLen = len(datagram) - DATAGRAM_SIZE
-	domainNameBites = datagram[13:13 + domainNameLen]
+	domainNameBytes = datagram[13:13 + domainNameLen]
 	domainName = []
 
-	for c in domainNameBites:
+	for c in domainNameBytes:
 		if c == 3:
-			domainName.append(46)
+			domainName.append('.')
 		else:
-			domainName.append(c)
-
-	print("Domain Name [", domainName, "] ")
+			domainName.append(chr(c))
+	domainName = ''.join(domainName)
+	return domainName
 
 def acceptData():
 	socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	socketUDP.bind(('127.0.0.1', DEAFAUL_PORT))
 	datagram, address = socketUDP.recvfrom(MAX_DATAGRAM_LEN)
-	print("Conected with: ", address)
-	print("Datagram: ", datagram)
+	# print("Conected with: ", address)
+	# print("Datagram: ", datagram)
 	socketUDP.sendto(datagram, address)
-	getDomainName(datagram)
+	domainName = getDomainName(datagram)
+	readSettings(domainName)
 	socketUDP.close()
 
 def main():
-	print("Hello world!")
 	acceptData()
 
 main()
