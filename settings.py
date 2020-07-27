707b925947fe8790a573f76e8d2e4c47b404442e
Kristina <kristina3138040@gmail.com>
2020-07-27 13:33:21 +0300

    Add setting class

---
+++ settings.py
@@ -0,0 +1,13 @@
+
+import yaml
+
+class Settings:
+	def __init__(self, DNSdatagram, address, sock):
+		with open('DNSProxyServer.conf') as f:
+			config = yaml.safe_load(f)
+		self.blacklist = config["blacklist"]
+		self.superior_dns_server = config["superior_dns_server"]
+		self.ban_msg = config["ban_msg"]
+		self.datagram = DNSdatagram
+		self.UDPsocket_addr = address
+		self.UDPsocket = sock
